74686c9b6badb7a56d711cff0123192cc200d5da
mariafdacevedo <113567795+mariafdacevedo@users.noreply.github.com>
2022-11-24 10:00:34 -0500

    Repositorios-Mafe

--- ApiG46BD/src/main/java/com/example/ApiG46BD/Repositorios/PartidoRepositorio.java
+++ ApiG46BD/src/main/java/com/example/ApiG46BD/Repositorios/PartidoRepositorio.java
@@ -1,5 +1,13 @@
 package com.example.ApiG46BD.Repositorios;
 
-public class PartidoRepositorio {
-    
+import java.util.ArrayList;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.example.ApiG46BD.Modelos.PartidoModelo;
+
+public interface PartidoRepositorio extends MongoRepository<PartidoModelo, Long> {
+    ArrayList<PartidoModelo> findBycategoria (String categoria);
+    ArrayList<PartidoModelo> findByevento (String evento);
+    ArrayList<PartidoModelo> findByequipo (String equipo);   
 }
