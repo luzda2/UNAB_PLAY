e4352884dbb54d32281c0f56a1dbfec14563e63c
Juanes0430 <113567807+Juanes0430@users.noreply.github.com>
2022-11-25 21:58:45 -0500

    Creación clase partido

---
+++ ApiG46BD/src/main/java/com/example/ApiG46BD/Servicios/PartidoServicio.java
@@ -0,0 +1,45 @@
+package com.example.ApiG46BD.Servicios;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+public class PartidoServicio {
+    @Service
+    public class PartidoServicio {
+        // instanciamos modelos de repository para usar.
+        @Autowired
+        PartidoRepositorio repositorio;
+
+        public PartidoModelo guardaPartido(PartidoModelo partido){
+            // metodo guardar dato patido, despues de public se escribe que retorna el metodo, en el parentesis lo que recibe
+            //retorna un partido modelo y recibe un partido modelo para almacenar en mongo
+            return repositorio.save(partido);
+            //metodo pre hecho del repositorio que guarda los datos que manda el cliente del usuario
+        }
+
+        //metodos para buscar un partido por tipo de dato (categoría, evento o equipo)
+        public ArrayList<PartidoModelo>  buscarPartidoxCategoria(){
+            return (ArrayList<PartidoModelo>) repositorio.findById(categoria);
+        }
+
+        public ArrayList<PartidoModelo> buscarPartidoxEvento(){
+            return (ArrayList<PartidoModelo>) repositorio.findById(evento);
+        }
+        
+        public ArrayList<PartidoModelo> buscarPartidoxEquipo(){
+            return (ArrayList<PartidoModelo>) repositorio.findById(equipo);
+        }
+
+        //metodo para eliminar un partido, returna un boolena. Si elimino true, si no elimino false.
+        public boolean eliminarUsuario(Long idPartido){
+            if (repositorio.existsById(idPartido)){
+                    repositorio.deleteById(idPartido);
+                    return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+}
