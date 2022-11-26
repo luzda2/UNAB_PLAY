40faacfb05e8601ebc13c7b49e35de2d54c52d6d
Juanes0430 <113567807+Juanes0430@users.noreply.github.com>
2022-11-25 22:05:59 -0500

    Creación clase servicio

--- ApiG46BD/src/main/java/com/example/ApiG46BD/Servicios/PartidoServicio.java
+++ ApiG46BD/src/main/java/com/example/ApiG46BD/Servicios/PartidoServicio.java
@@ -4,42 +4,44 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.ApiG46BD.Modelos.PartidoModelo;
+import com.example.ApiG46BD.Repositorios.PartidoRepositorio;
+
+
+@Service
 public class PartidoServicio {
-    @Service
-    public class PartidoServicio {
-        // instanciamos modelos de repository para usar.
-        @Autowired
-        PartidoRepositorio repositorio;
+    // instanciamos modelos de repository para usar.
+    @Autowired
+    PartidoRepositorio repositorio;
 
-        public PartidoModelo guardaPartido(PartidoModelo partido){
-            // metodo guardar dato patido, despues de public se escribe que retorna el metodo, en el parentesis lo que recibe
-            //retorna un partido modelo y recibe un partido modelo para almacenar en mongo
-            return repositorio.save(partido);
-            //metodo pre hecho del repositorio que guarda los datos que manda el cliente del usuario
-        }
+    public PartidoModelo guardaPartido(PartidoModelo partido){
+        // metodo guardar dato patido, despues de public se escribe que retorna el metodo, en el parentesis lo que recibe
+        //retorna un partido modelo y recibe un partido modelo para almacenar en mongo
+        return repositorio.save(partido);
+        //metodo pre hecho del repositorio que guarda los datos que manda el cliente del usuario
+    }
 
-        //metodos para buscar un partido por tipo de dato (categoría, evento o equipo)
-        public ArrayList<PartidoModelo>  buscarPartidoxCategoria(){
-            return (ArrayList<PartidoModelo>) repositorio.findById(categoria);
-        }
+    //metodos para buscar un partido por tipo de dato (categoría, evento o equipo)
+    public ArrayList<PartidoModelo>  buscarPartidoxCategoria(){
+        return (ArrayList<PartidoModelo>) repositorio.findById(categoria);
+    }
 
-        public ArrayList<PartidoModelo> buscarPartidoxEvento(){
-            return (ArrayList<PartidoModelo>) repositorio.findById(evento);
-        }
-        
-        public ArrayList<PartidoModelo> buscarPartidoxEquipo(){
-            return (ArrayList<PartidoModelo>) repositorio.findById(equipo);
-        }
+    public ArrayList<PartidoModelo> buscarPartidoxEvento(){
+        return (ArrayList<PartidoModelo>) repositorio.findById(evento);
+    }
+    
+    public ArrayList<PartidoModelo> buscarPartidoxEquipo(){
+        return (ArrayList<PartidoModelo>) repositorio.findById(equipo);
+    }
 
-        //metodo para eliminar un partido, returna un boolena. Si elimino true, si no elimino false.
-        public boolean eliminarUsuario(Long idPartido){
-            if (repositorio.existsById(idPartido)){
-                    repositorio.deleteById(idPartido);
-                    return true;
-            }
-            else{
-                return false;
-            }
+    //metodo para eliminar un partido, returna un boolena. Si elimino true, si no elimino false.
+    public boolean eliminarUsuario(Long idPartido){
+        if (repositorio.existsById(idPartido)){
+                repositorio.deleteById(idPartido);
+                return true;
+        }
+        else{
+            return false;
         }
     }
 }
