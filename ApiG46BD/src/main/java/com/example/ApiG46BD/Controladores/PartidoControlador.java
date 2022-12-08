d124158f9fe90eb50b4431f829fcef3431d0699e
mariafdacevedo <113567795+mariafdacevedo@users.noreply.github.com>
2022-12-08 16:35:24 -0500

    Cross Origin

--- ApiG46BD/src/main/java/com/example/ApiG46BD/Controladores/PartidoControlador.java
+++ ApiG46BD/src/main/java/com/example/ApiG46BD/Controladores/PartidoControlador.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.example.ApiG46BD.Servicios.PartidoServicio;
 
 @RestController
 @RequestMapping("/partido")
+@CrossOrigin("*")
 //http://localhost:8080/partido
 public class PartidoControlador {
     @Autowired
