669c2eba50c03fd7d6fbaeb4bea17f3db99ea2c7
Luz Dary Carrero <est_ld_carrero@fesc.edu.co>
2022-12-09 20:45:45 -0500

    deploy

--- ApiG46BD/src/main/java/com/example/ApiG46BD/Controladores/UsuarioControlador.java
+++ ApiG46BD/src/main/java/com/example/ApiG46BD/Controladores/UsuarioControlador.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.example.ApiG46BD.Servicios.UsuarioServicio;
 
 @RestController
 @RequestMapping("/usuario")
+@CrossOrigin("*")
 //http://localhost:8080/usuario
 public class UsuarioControlador {
     @Autowired
